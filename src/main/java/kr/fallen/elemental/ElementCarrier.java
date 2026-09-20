2fd443b953471f01d7c58c75b646c74e9c496f01
chohyunbin305-creator <chohyunbin305@gmail.com>
2026-09-21 05:32:12 +0900

    Add Elemental Eyes source

---
+++ src/main/java/kr/fallen/elemental/ElementCarrier.java
@@ -0,0 +1,10 @@
+package kr.fallen.elemental;
+public interface ElementCarrier {
+    Element elementalEyes$getElement();
+    int elementalEyes$getElementTicks();
+    void elementalEyes$setElement(Element e, int ticks);
+    void elementalEyes$clearElement();
+    String elementalEyes$getReaction();
+    int elementalEyes$getReactionTicks();
+    void elementalEyes$setReaction(String id, int ticks);
+}
