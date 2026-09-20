32816d84ee0f05acc282710c36f63b3b08d88242
chohyunbin305-creator <chohyunbin305@gmail.com>
2026-09-21 05:32:09 +0900

    Add Elemental Eyes source

---
+++ src/main/java/kr/fallen/elemental/Element.java
@@ -0,0 +1,6 @@
+package kr.fallen.elemental;
+public enum Element {
+    FIRE("fire"), WATER("water"), ELECTRIC("electric"), WIND("wind"), EARTH("earth");
+    public final String id;
+    Element(String id){this.id=id;}
+}
