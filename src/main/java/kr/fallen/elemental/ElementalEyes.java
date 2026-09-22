31ee7b2821f9c96266ed92a31301bbfda5175298
chohyunbin305-creator <chohyunbin305@gmail.com>
2026-09-22 21:24:12 +0900

    Fix prototype compilation

--- src/main/java/kr/fallen/elemental/ElementalEyes.java
+++ src/main/java/kr/fallen/elemental/ElementalEyes.java
@@ -1,25 +1,11 @@
 package kr.fallen.elemental;
-
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Identifier;
-
 public class ElementalEyes implements ModInitializer {
  public static final String MODID="elemental_eyes";
- public static final Identifier SKILL1=Identifier.of(MODID,"skill1");
- public static final Identifier SKILL2=Identifier.of(MODID,"skill2");
  @Override public void onInitialize(){
-  ServerPlayNetworking.registerGlobalReceiver(SKILL1,(server,player,h,r)->server.execute(()->ToadSage.skillOil(player)));
-  ServerPlayNetworking.registerGlobalReceiver(SKILL2,(server,player,h,r)->server.execute(()->ToadSage.skillFire(player)));
-  ServerTickEvents.END_SERVER_TICK.register(server->server.getPlayerManager().getPlayerList().forEach(ToadSage::tick));
-  AttackEntityCallback.EVENT.register((player,world,hand,entity,hit)->{
-   if(!world.isClient&&entity instanceof LivingEntity target) AttributeSystem.hit(player,target,null);
-   return ActionResult.PASS;
-  });
+  AttackEntityCallback.EVENT.register((player,world,hand,entity,hit)->{if(!world.isClient&&entity instanceof LivingEntity target)AttributeSystem.hit(player,target,null);return ActionResult.PASS;});
  }
 }
--- src/main/java/kr/fallen/elemental/client/ElementalEyesClient.java
+++
@@ -1,7 +0,0 @@
-package kr.fallen.elemental.client;
-import kr.fallen.elemental.ElementalEyes;
-import net.fabricmc.api.ClientModInitializer;import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;import net.minecraft.client.option.KeyBinding;import net.minecraft.client.util.InputUtil;import net.minecraft.network.PacketByteBuf;import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;import org.lwjgl.glfw.GLFW;
-public class ElementalEyesClient implements ClientModInitializer{
- private KeyBinding s1,s2;
- public void onInitializeClient(){s1=KeyBindingHelper.registerKeyBinding(new KeyBinding("key.elemental_eyes.skill1",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_R,"category.elemental_eyes"));s2=KeyBindingHelper.registerKeyBinding(new KeyBinding("key.elemental_eyes.skill2",InputUtil.Type.KEYSYM,GLFW.GLFW_KEY_V,"category.elemental_eyes"));ClientTickEvents.END_CLIENT_TICK.register(c->{while(s1.wasPressed())ClientPlayNetworking.send(ElementalEyes.SKILL1,PacketByteBufs.empty());while(s2.wasPressed())ClientPlayNetworking.send(ElementalEyes.SKILL2,PacketByteBufs.empty());});}
-}
